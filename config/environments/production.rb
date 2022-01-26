f89562e1c0ece120a03aa310265a24a2078b7549
ryusei1212 <black245k@gmail.com>
2022-01-26 09:53:00 +0900

    Mailgunアドオン設定のコメントアウト解除

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,18 +68,18 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   # アプリケーションでMailgunアドオンを使う設定
-  # config.action_mailer.raise_delivery_errors = true
-  # config.action_mailer.delivery_method = :test
-  # host = 'make-typing-game.herokuapp.com'
-  # config.action_mailer.default_url_options = { host: host }
-  # ActionMailer::Base.smtp_settings = {
-  #   :port           => ENV['MAILGUN_SMTP_PORT'],
-  #   :address        => ENV['MAILGUN_SMTP_SERVER'],
-  #   :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-  #   :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-  #   :domain         => host,
-  #   :authentication => :plain,
-  # }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :test
+  host = 'make-typing-game.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
+  ActionMailer::Base.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => host,
+    :authentication => :plain,
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
