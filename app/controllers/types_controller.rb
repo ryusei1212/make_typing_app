501ece270743f217ac99b437e0ff32b1ab6fdac6
ryusei1212 <black245k@gmail.com>
2023-03-07 19:12:04 +0900

    不要な記述の削除

--- app/controllers/types_controller.rb
+++ app/controllers/types_controller.rb
@@ -1,13 +1,9 @@
 class TypesController < ApplicationController
   def index
-    title_query_id = params[:title_id]
-    @text = Text.where(title_id: title_query_id).pluck(:sentence)
-    @text_json = @text.to_json
+    @text = Text.where(title_id: params[:title_id]).pluck(:sentence)
   end
 
   def nomal
-    title_query_id = params[:title_id]
-    @text = Text.where(title_id: title_query_id).pluck(:sentence)
-    @text_json = @text.to_json
+    @text = Text.where(title_id: params[:title_id]).pluck(:sentence)
   end
 end
--- app/models/text.rb
+++ app/models/text.rb
@@ -1,11 +1,4 @@
 class Text < ApplicationRecord
   belongs_to :title
   validates :sentence, presence: true, length: { maximum: 255}
-
-  # def self.multi_update(texts_params)
-  #   texts_params.to_h.map do |id, text_param|
-  #     text = self.find(id)
-  #     text.update!(text_param)
-  #   end
-  # end
 end
